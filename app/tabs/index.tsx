f8266943803aedc198762f0b3ee8da0ad316a819
smw22 <sergiomoronwestergaard@gmail.com>
2026-04-09 15:38:34 +0200

    feat: integrate gesture handling for emoji scaling and dragging in EmojiSticker component

--- app/tabs/index.tsx
+++ app/tabs/index.tsx
@@ -1,6 +1,7 @@
 import * as ImagePicker from "expo-image-picker";
 import { useState } from "react";
 import { ImageSourcePropType, StyleSheet, View } from "react-native";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 
 import Button from "@/components/Button";
 import CircleButton from "@/components/CircleButton";
@@ -54,7 +55,7 @@ export default function Index() {
   };
 
   return (
-    <View style={styles.container}>
+    <GestureHandlerRootView style={styles.container}>
       <View style={styles.imageContainer}>
         <ImageViewer
           imgSource={PlaceholderImage}
@@ -92,7 +93,7 @@ export default function Index() {
       <EmojiPicker isVisible={isModalVisible} onClose={onModalClose}>
         <EmojiList onSelect={setPickedEmoji} onCloseModal={onModalClose} />
       </EmojiPicker>
-    </View>
+    </GestureHandlerRootView>
   );
 }
 
