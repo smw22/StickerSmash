74171dae7c4434428cf24569bf2de68b705b1e73
smw22 <sergiomoronwestergaard@gmail.com>
2026-04-09 13:53:01 +0200

    feat: add image picker functionality to Index screen and update ImageViewer component

--- app/tabs/index.tsx
+++ app/tabs/index.tsx
@@ -1,18 +1,45 @@
-import { StyleSheet, View } from "react-native";
-
 import Button from "@/components/Button";
 import ImageViewer from "@/components/ImageViewer";
+import * as ImagePicker from "expo-image-picker";
+import { useState } from "react";
+import { StyleSheet, View } from "react-native";
 
 const PlaceholderImage = require("@/assets/images/background-image.png");
 
 export default function Index() {
+  const [selectedImage, setSelectedImage] = useState<string | undefined>(
+    undefined,
+  );
+
+  const pickImageAsync = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ["images"],
+      allowsEditing: true,
+      quality: 1,
+    });
+
+    if (!result.canceled) {
+      console.log(result);
+      setSelectedImage(result.assets[0].uri);
+    } else {
+      alert("You did not select any image.");
+    }
+  };
+
   return (
     <View style={styles.container}>
       <View style={styles.imageContainer}>
-        <ImageViewer imgSource={PlaceholderImage} />
+        <ImageViewer
+          imgSource={PlaceholderImage}
+          selectedImage={selectedImage}
+        />
       </View>
       <View style={styles.footerContainer}>
-        <Button label="Choose a photo" theme="primary" />
+        <Button
+          label="Choose a photo"
+          theme="primary"
+          onPress={pickImageAsync}
+        />
         <Button label="Use this photo" />
       </View>
     </View>
